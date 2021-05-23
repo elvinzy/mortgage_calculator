ad0d60a185745a719f43ffa2076fa7b67ac484b8
echua <echua@micron.com>
2021-05-23 17:04:44 +0800

    Minor update (docstring)

--- mortgage_calc.py
+++ mortgage_calc.py
@@ -2,7 +2,7 @@
 """
 Created on Sun May 23 16:02:02 2021
 
-@author: echua
+@author: elvinzy
 """
 # Import packages
 import pandas as pd
@@ -18,21 +18,21 @@ interest_month = interest_year / 100 / 12
 
 # Formula to calculate monthly repayment
 month_payment = loan_amount * ((interest_month * (1 + interest_month) ** loan_month) / (((1 + interest_month) ** loan_month) -1))
-month_payment = round(month_payment,2)
+month_payment = round(month_payment, 2)
 
 print('')
 print('Your monthly payment is $' + str(month_payment))
 
-# Plot
+# Plot the remaining amount against month
 month = []
 remaining_amount = []
-for i in range(1,loan_month+1):
+for i in range(1, loan_month+1):
     interest = loan_amount * interest_month
     loan_amount = loan_amount - (month_payment - interest)
     month.append(i)
     remaining_amount.append(loan_amount)
 
-plt.plot(month, remaining_amount,'ro')
+plt.plot(month, remaining_amount, 'ro')
 plt.xlabel('Month')
 plt.ylabel('Remaining Amount ($)')
 plt.show()
